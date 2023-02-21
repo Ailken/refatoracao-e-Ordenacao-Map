f45630e55a92a906aded7fc812b76bd956c27fe5
Ailken <ailkencf@gmail.com>
2023-02-20 23:25:02 -0300

    Exercicio proposto de refatoração e Ordenação Map 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.function.Function;
 
 public class Main {
 /* Dadas as seguintes informações de id e contato, crie um dicionário e ordene este diccionário exibindo (Nome id - Nome contato);
@@ -39,7 +40,25 @@ id = 3 - Contato = nome; Dani, numero: 1111;
 
         System.out.println("--\tOrdem número telefone\t--");
         //precisamos organizar os valores. Logo:
-        Set<Map.Entry<Integer, Contato>> set = new TreeSet<>(new ComparatorOrdemNumerica());
+
+        /*Set<Map.Entry<Integer, Contato>> set = new TreeSet<>(new Comparator<Map.Entry<Integer, Contato>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Contato> cont1, Map.Entry<Integer, Contato> cont2) {
+                return Integer.compare(cont1.getValue().getNumero(), cont2.getValue().getNumero());
+            }
+        });*/
+
+        /*Set<Map.Entry<Integer, Contato>> set = new TreeSet<>(Comparator.comparing(new Function<Map.Entry<Integer,Contato>, Integer>() {
+            @Override
+            public Integer apply(Map.Entry<Integer, Contato> cont) {
+                return cont.getValue().getNumero();
+            }
+        }));*/
+
+        Set<Map.Entry<Integer, Contato>> set = new TreeSet<>(Comparator.comparing(
+                cont -> cont.getValue().getNumero()));
+
+
         set.addAll(agenda.entrySet());
         for (Map.Entry<Integer, Contato> entry: set) {
             System.out.println(entry.getKey() + " - " + entry.getValue().getNumero() +
@@ -56,12 +75,12 @@ id = 3 - Contato = nome; Dani, numero: 1111;
     }
 }
 
-class ComparatorOrdemNumerica implements Comparator<Map.Entry<Integer, Contato>> {
+/*class ComparatorOrdemNumerica implements Comparator<Map.Entry<Integer, Contato>> {
     @Override
     public int compare(Map.Entry<Integer, Contato> cont1, Map.Entry<Integer, Contato> cont2) {
         return Integer.compare(cont1.getValue().getNumero(), cont2.getValue().getNumero());
     }
-}
+}*/
 
 class ComparatorOrdemNomeContato implements Comparator<Map.Entry<Integer, Contato>> {
     @Override
